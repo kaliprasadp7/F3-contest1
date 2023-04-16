c7f7f6c5a42596f4f85e34cf28f26f431efa064b
Kali Prasad Panda <kalip.panda@nettantra.net>
2023-04-16 11:16:17 +0530

    updated js for success error and empty message

--- main.js
+++ main.js
@@ -10,17 +10,32 @@ userArray=JSON.parse(objstr)||[];
 console.log(userArray);
 displayInfo();
 
+if(userArray.length==0){
+    document.getElementById("empty").style.display="block";
+}else{
+    document.getElementById("empty").style.display="none";
+}
+
 btn.addEventListener("click", function(){
     let name=username.value;
     let profession=userprofession.value;
     let age=userage.value;
 
+    
     if(name != '' && profession != '' && age != ''){
         userArray.push({'name' : name, 'profession' : profession, 'age' : age});
         document.getElementById("error").style.display="none";
         document.getElementById("success").style.display="block";
+        document.getElementById("empty").style.display="none";
+        username.value='';
+        userprofession.value='';
+        userage.value='';
     }else{
+        document.getElementById("success").style.display="none";
         document.getElementById("error").style.display="block";
+        if(userArray.length==0){
+            document.getElementById("empty").style.display="block";
+        }
     }
 
     saveInfo(userArray);
@@ -52,4 +67,11 @@ function displayInfo(){
 function deleteInfo(i){
     userArray.splice(i,1);
     saveInfo(userArray);
+    document.getElementById("success").style.display="none";
+    document.getElementById("error").style.display="none";
+    if(userArray.length==0){
+        document.getElementById("empty").style.display="block";
+    }else{
+        document.getElementById("empty").style.display="none";
+    }
 }
